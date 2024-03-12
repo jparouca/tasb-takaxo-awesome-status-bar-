eb6205dcea92849ff08481c35c49b8c3f560513c
jparouca <joaopearoucaa@gmail.com>
2024-03-12 17:39:48 -0300

    workspaces, mpris playe, pulseuadio volume and systray

--- main.ts
+++ main.ts
@@ -1,31 +1,148 @@
-const time = Variable('', {
-    poll: [1000, function() {
-        return Date().toString();
-    }],
-});
+const hyprland = await Service.import("hyprland")
+const mpris = await Service.import("mpris")
+const notifications = await Service.import("notifications")
+const systemtray = await Service.import("systemtray")
+const audio = await Service.import("audio")
 
-const idle = Variable(0)
-idle.connect('changed', ({ value }) => {
-    print(`idle changed: ${value}`);
-})
+function Left() {
+    return Widget.Box({
+        spacing: 8,
+        children: [
+            Workspaces(),
+        ],
+    })
+}
 
-const pactl = Variable({ count: 0, msg: '' }, {
-    listen: ['pactl subscribe', (msg) => ({
-        count: pactl.value.count + 1,
-        msg: msg,
-    })],
-})
-pactl.connect('changed', ({ value }) => {
-    print(value.msg, value.count)
-})
-const penis = Widget.Label({
-    label: pactl.bind().as(({ count, msg }) => {
-        return `${msg} ${count}`
-    }),
-})
-penis.connect('notify::label', ({ label }) => {
-    print('label changed to ', label)
-})
+function Workspaces() {
+    const activeId = hyprland.active.workspace.bind("id")
+    const workspaces = hyprland.bind("workspaces")
+        .as(ws => ws.map(({ id }) => Widget.Button({
+            on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
+            child: Widget.Label(`${id}`),
+            class_name: activeId.as(i => `${i === id ? "focused" : ""}`),
+        })))
+
+    return Widget.Box({
+        class_name: "workspaces",
+        children: workspaces,
+    })
+}
+
+
+function Center() {
+    return Widget.Box({
+        spacing: 8,
+        children: [
+            Media(),
+            Notification(),
+        ],
+    })
+}
+
+
+function Media() {
+    const label = Utils.watch("", mpris, "player-changed", () => {
+        if (mpris.players[0]) {
+            const { track_artists, track_title } = mpris.players[0]
+            return `${track_artists.join(", ")} - ${track_title}`
+        } else {
+            return "Nothing is playing"
+        }
+    })
+
+    return Widget.Button({
+        class_name: "media",
+        on_primary_click: () => mpris.getPlayer("")?.playPause(),
+        on_scroll_up: () => mpris.getPlayer("")?.next(),
+        on_scroll_down: () => mpris.getPlayer("")?.previous(),
+        child: Widget.Label({ label }),
+    })
+}
+
+// we don't need dunst or any other notification daemon because the Notifications module is a notification daemon itself xD
+function Notification() {
+    const popups = notifications.bind("popups")
+    return Widget.Box({
+        class_name: "notification",
+        visible: popups.as(p => p.length > 0),
+        children: [
+            Widget.Icon({
+                icon: "preferences-system-notifications-symbolic",
+            }),
+            Widget.Label({
+                label: popups.as(p => p[0]?.summary || ""),
+            }),
+        ],
+    })
+}
+
+function Right() {
+    return Widget.Box({
+        hpack: "end",
+        spacing: 8,
+        children: [
+            Volume(),
+            SysTray(),
+        ],
+    })
+}
+
+function Volume() {
+    const icons = {
+        101: "overamplified",
+        67: "high",
+        34: "medium",
+        1: "low",
+        0: "muted",
+    }
+
+    function getIcon() {
+        const icon = audio.speaker.is_muted ? 0 : [101, 67, 34, 1, 0].find(
+            threshold => threshold <= audio.speaker.volume * 100)
+
+        return `audio-volume-${icons[icon]}-symbolic`
+    }
+
+    const icon = Widget.Icon({
+        icon: Utils.watch(getIcon(), audio.speaker, getIcon),
+    })
+
+    const slider = Widget.Slider({
+        hexpand: true,
+        draw_value: false,
+        on_change: ({ value }) => audio.speaker.volume = value,
+        setup: self => self.hook(audio.speaker, () => {
+            self.value = audio.speaker.volume || 0
+        }),
+    })
+
+    return Widget.Box({
+        class_name: "volume",
+        css: "min-width: 180px",
+        children: [icon, slider],
+    })
+}
+
+function Clock() {
+    return Widget.Label({
+        class_name: "clock",
+        label: date.bind(),
+    })
+}
+
+function SysTray() {
+    const items = systemtray.bind("items")
+        .as(items => items.map(item => Widget.Button({
+            child: Widget.Icon({ icon: item.bind("icon") }),
+            on_primary_click: (_, event) => item.activate(event),
+            on_secondary_click: (_, event) => item.openMenu(event),
+            tooltip_markup: item.bind("tooltip_markup"),
+        })))
+
+    return Widget.Box({
+        children: items,
+    })
+}
 
 
 const Bar = (monitor: number = 0) => Widget.Window({
@@ -34,19 +151,9 @@ const Bar = (monitor: number = 0) => Widget.Window({
     anchor: ['top', 'left', 'right'],
     exclusivity: 'exclusive',
     child: Widget.CenterBox({
-        start_widget: Widget.Label({
-            hpack: 'center',
-            label: 'Welcome to AGS!',
-        }),
-        center_widget: Widget.Label({
-            hpack: 'center',
-            label: penis,
-
-        }),
-        end_widget: Widget.Label({
-            hpack: 'center',
-            label: time.bind(),
-        }),
+        start_widget: Left(),
+        center_widget: Center(),
+        end_widget: Right(),
     }),
 });
 
