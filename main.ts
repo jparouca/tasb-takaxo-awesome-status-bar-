6b1162c9a7bd6655eafceeadd772d8f767c0b181
jparouca <joaopearoucaa@gmail.com>
2024-03-13 18:09:55 -0300

    audio widget

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
-import AIPopup from "AI/AIPopup"
+import { Audio } from "Widgets/Audio"
 import { Clock } from "Widgets/Clock"
 
 const hyprland = await Service.import("hyprland")
 const mpris = await Service.import("mpris")
 const notifications = await Service.import("notifications")
 const systemtray = await Service.import("systemtray")
-const audio = await Service.import("audio")
 
 
 function Left() {
@@ -88,49 +87,49 @@ function Right() {
     hpack: "end",
     spacing: 8,
     children: [
-      Volume(),
+      Audio,
       SysTray(),
       Clock,
     ],
   })
 }
 
-function Volume() {
-  const icons = {
-    101: "overamplified",
-    67: "high",
-    34: "medium",
-    1: "low",
-    0: "muted",
-  }
-
-  function getIcon() {
-    const icon = audio.speaker.is_muted ? 0 : [101, 67, 34, 1, 0].find(
-      threshold => threshold <= audio.speaker.volume * 100)
-
-    return `audio-volume-${icons[icon]}-symbolic`
-  }
-
-  const icon = Widget.Icon({
-    icon: Utils.watch(getIcon(), audio.speaker, getIcon),
-  })
-
-  const slider = Widget.Slider({
-    hexpand: true,
-    draw_value: false,
-    on_change: ({ value }) => audio.speaker.volume = value,
-    setup: self => self.hook(audio.speaker, () => {
-      self.value = audio.speaker.volume || 0
-    }),
-  })
-
-  return Widget.Box({
-    class_name: "volume",
-    css: "min-width: 180px",
-    children: [icon, slider],
-  })
-}
-
+// function Volume() {
+//   const icons = {
+//     101: "overamplified",
+//     67: "high",
+//     34: "medium",
+//     1: "low",
+//     0: "muted",
+//   }
+//
+//   function getIcon() {
+//     const icon = audio.speaker.is_muted ? 0 : [101, 67, 34, 1, 0].find(
+//       threshold => threshold <= audio.speaker.volume * 100)
+//
+//     return `audio-volume-${icons[icon]}-symbolic`
+//   }
+//
+//   const icon = Widget.Icon({
+//     icon: Utils.watch(getIcon(), audio.speaker, getIcon),
+//   })
+//
+//   const slider = Widget.Slider({
+//     hexpand: true,
+//     draw_value: false,
+//     on_change: ({ value }) => audio.speaker.volume = value,
+//     setup: self => self.hook(audio.speaker, () => {
+//       self.value = audio.speaker.volume || 0
+//     }),
+//   })
+//
+//   return Widget.Box({
+//     class_name: "volume",
+//     css: "min-width: 180px",
+//     children: [icon, slider],
+//   })
+// }
+//
 function SysTray() {
   const items = systemtray.bind("items")
     .as(items => items.map(item => Widget.Button({
