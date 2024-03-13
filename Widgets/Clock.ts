eb4800f852cecffcce6053c55d151784ae41b309
jparouca <joaopearoucaa@gmail.com>
2024-03-13 19:41:06 -0300

    calendar widget overlay to onclick clock

--- Widgets/Clock.ts
+++ Widgets/Clock.ts
@@ -1,4 +1,7 @@
+import Menu from './menu/Menu.js'
 export const Clock = Widget.Button({
+  onClicked: (self) => self.attribute.menu.toggle(),
+  attribute: { menu: Menu('calendar') },
   className: 'clock',
   child: Widget.Label().poll(30000, (self) =>
     self.label = Utils.exec(`date +'%H:%M'`)
